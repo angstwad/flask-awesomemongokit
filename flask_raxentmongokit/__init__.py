101288b191b8ebfc7f86f71fd714067270fe1439
Paul Durivage <pauldurivage@gmail.com>
2015-03-13 12:15:01 -0500

    Remove this stuff

--- flask_raxentmongokit/__init__.py
+++ flask_raxentmongokit/__init__.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '0.3'
-
-from raxentmongokit import RaxEntMongokit  # noqa
-from index import index_all_docs  # noqa
+__version__ = '0.3.2'
